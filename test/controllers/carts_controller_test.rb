6dbb128358b96ff21253dbc351b991fea17d107e
Damian Szalbierz <szalbierz.d.k@gmail.com>
2016-08-30 13:00:43 +0000

    Create cart in Sidebar

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -27,7 +27,7 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
     get cart_url(@cart)
     assert_response :success
   end
-
+  
   test "should get edit" do
     get edit_cart_url(@cart)
     assert_response :success
